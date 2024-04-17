6273e23556eef342eb1d198ede6bc69891f5fba1
pushpendrasingh001 <pushpendrasingh1919@gmail.com>
2024-04-17 16:43:25 +0530

    favicon update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import StartGame from "./components/StartGame";
 import GamePlay from "./components/GamePlay";
+import './App.css'
 
 function App() {
   const [isGameStarted, setIsGameStarted] = useState(false);
@@ -10,7 +11,9 @@ function App() {
   };
 
   return (
-    <>{isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}</>
+    <>{isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}
+  <p className="copy_right">  All Right Reserved by: Pushpendra Singh</p>
+    </>
   );
 }
 
